d42dfacafdfddc19dc5eb07e1ae36dfb61cff69f
Ethan Lee <elee@xpanxion.com>
2023-07-05 14:56:09 -0400

    Finish ex2

--- src/ex2.py
+++ src/ex2.py
@@ -1,6 +1,18 @@
-
+import csv
 def find_total_visits():
-    pass # TODO
+    result = 0
+    filenames = ["../files/week-1.csv", "../files/week-2.csv", "../files/week-3.csv"]
+    for filename in filenames:
+        with open(filename, "r") as file:
+            next(file)
+            lines = csv.reader(file, delimiter=',')
+            for line in lines:
+                # print(line)
+                for value in line[1:]:
+                    if int(value) == 1:
+                        result += 1
+    return result
+
 
 
 def ex2():
